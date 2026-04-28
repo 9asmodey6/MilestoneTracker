ff6d2837acb4cda5a24f936be5d123a969b7c5c2
9asmodey6 <longjorney1@gmail.com>
2026-04-28 14:04:52 +0300

    Refactored program.cs

--- MilestoneTracker.API/Program.cs
+++ MilestoneTracker.API/Program.cs
@@ -1,8 +1,6 @@
 using MilestoneTracker.Application;
 using MilestoneTracker.Infrastructure;
-using MilestoneTracker.Infrastructure.Options;
 using Serilog;
-using Telegram.Bot;
 using MilestoneTracker.API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -22,61 +20,17 @@ var app = builder.Build();
 
 app.UseSerilogRequestLogging(options =>
 {
-    options.MessageTemplate = "HTTP {RequestMethod} {RequestPath} responded {StatusCode} in {Elapsed:0.0000} ms. User: {UserId}";
+    options.MessageTemplate =
+        "HTTP {RequestMethod} {RequestPath} responded {StatusCode} in {Elapsed:0.0000} ms. User: {UserId}";
 });
 
 
 if (app.Environment.IsDevelopment())
 {
-    
     app.ApplyMigrations();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.MapControllers();
-
-using (var scope = app.Services.CreateScope())
-{
-    var botClient = scope.ServiceProvider.GetRequiredService<ITelegramBotClient>();
-    var options = builder.Configuration.GetSection("Telegram").Get<TelegramOptions>();
-
-    if (!string.IsNullOrEmpty(options?.WebhookUrl))
-    {
-        try
-        {
-            await botClient.DeleteWebhook();
-            Console.WriteLine("🗑️ Старый webhook удалён");
-
-            await botClient.SetWebhook(
-                url: options.WebhookUrl,
-                cancellationToken: default);
-            
-            Console.WriteLine($"Webhook установлен на: {options.WebhookUrl}");
-            
-            var webhookInfo = await botClient.GetWebhookInfo(
-                cancellationToken: default);
-            
-            Console.WriteLine($"  Webhook Info:");
-            Console.WriteLine($"   URL: {webhookInfo.Url}");
-            Console.WriteLine($"   Pending Updates: {webhookInfo.PendingUpdateCount}");
-            if (!string.IsNullOrEmpty(webhookInfo.LastErrorMessage))
-            {
-                Console.WriteLine($"    Last Error: {webhookInfo.LastErrorMessage}");
-                Console.WriteLine($"   Last Error Date: {webhookInfo.LastErrorDate}");
-            }
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($" Ошибка установки webhook: {ex.Message}");
-            Console.WriteLine($"   Stack: {ex.StackTrace}");
-        }
-    }
-    else
-    {
-        Console.WriteLine(" WebhookUrl не настроен в конфигурации!");
-    }
-}
-
-
 app.Run();
