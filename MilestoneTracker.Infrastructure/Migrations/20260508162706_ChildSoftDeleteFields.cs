a894c6d20288c1f82f3679c63b0eae99c812e2d9
9asmodey6 <longjorney1@gmail.com>
2026-05-08 19:32:04 +0300

    Added soft deletion fields to children table

---
+++ MilestoneTracker.Infrastructure/Migrations/20260508162706_ChildSoftDeleteFields.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MilestoneTracker.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChildSoftDeleteFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "deleted_by",
+                table: "milestones",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "deleted_at",
+                table: "children",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "deleted_by",
+                table: "children",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "is_deleted",
+                table: "children",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "deleted_at",
+                table: "children");
+
+            migrationBuilder.DropColumn(
+                name: "deleted_by",
+                table: "children");
+
+            migrationBuilder.DropColumn(
+                name: "is_deleted",
+                table: "children");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "deleted_by",
+                table: "milestones",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+        }
+    }
+}
